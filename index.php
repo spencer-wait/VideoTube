ad2237839fcd8d8bedae9c934ff72b4e8d16230c
WarmBiscuit <spencer.wait@gmail.com>
2019-10-01 10:41:42 -0400

    Added stylesheet and created containers

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-test
+<!DOCTYPE html>
+<html> 
+<head>
+    <title>VideoTube</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+    
+    <div id="pageContainer">
+
+        <div id="mastHeadContainer">
+            hello
+        </div>
+
+        <div id="sideNavContainer">
+
+        </div>
+
+        <div id="mainSectionContainer">
+
+        </div>
+
+    </div>
+
+</body>
+</html>
