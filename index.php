f9d36639225adf0272e834de71a73e3e27c4b4a4
WarmBiscuit <spencer.wait@gmail.com>
2019-10-19 22:03:33 -0400

    Upload and Profile buttons; Created Header and Footer

--- index.php
+++ index.php
@@ -1,54 +1,5 @@
-<!DOCTYPE html>
-<html> 
-<head>
-    <title>VideoTube</title>
+<?php require_once("includes/header.php"); ?>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="assets/css/styleSheet.css">
+hello everyone
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="assets/js/commonActions.js"></script>
-
-</head>
-<body>
-    
-    <div id="pageContainer">
-
-        <div id="mastHeadContainer">
-            <button class="navShowHide">
-                <img src="assets/images/icons/menu.png">
-            </button>
-        
-            <a class="logoContainer" href="index.php">
-                <img src="assets/images/icons/VideoTubeLogo.png" title="VideoTube Logo" alt="Site Logo">
-            </a>
-
-            <div class="searchBarContainer">
-                <form action="search.php" method="GET">
-                    <input type="text" class="searchBar" name="term" placeholder="Search...">
-                    <button class="searchButton">
-                        <img src="assets/images/icons/search.png">
-                    </button>
-                </form>
-            </div>
-
-        </div>
-
-        <div id="sideNavContainer" style="display:none;">
-
-        </div>
-
-        <div id="mainSectionContainer">
-
-            <div id="mainContentContainer">
-                hello everyone
-            </div>
-            
-        </div>
-
-    </div>
-
-</body>
-</html>
+<?php require_once("includes/footer.php"); ?>        
