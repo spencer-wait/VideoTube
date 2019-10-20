04ad0dcfbc6293865e4dbb6cdbb66083163f4234
WarmBiscuit <spencer.wait@gmail.com>
2019-10-20 18:44:16 -0400

    Connected database and added video categories in db

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+ob_start(); // turns on output buffering
+
+date_default_timezone_set("America/New_York");
+
+try {
+    $con = new PDO("mysql:dbname=VideoTube;host=localhost", "root", "");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
+catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,4 @@
+<?php require_once("includes/config.php"); ?>
 <!DOCTYPE html>
 <html> 
 <head>
